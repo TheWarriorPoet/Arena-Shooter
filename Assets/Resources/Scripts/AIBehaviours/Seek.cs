4984d33d87aca74b0447e047e6526590fb6c668a
Mitchell Osborne <mitchell.osborne@students.aie.edu.au>
2015-06-18 18:41:00 +1000

    Added functionality for the Seek Command, Seek now moves the agent towards the first GameObject it finds with the "Player" tag.

--- Assets/Resources/Scripts/AIBehaviours/Seek.cs
+++ Assets/Resources/Scripts/AIBehaviours/Seek.cs
@@ -5,11 +5,12 @@ public class Seek : Behaviour {
 	Vector3 targetPos;
 	// Use this for initialization
 	public Seek(Agent aAgent) : base(aAgent) {
-		targetPos = Vector3.zero;
+		targetPos = GameObject.FindGameObjectWithTag("Player").transform.position;
 	}
 	
 	// Update is called once per frame
 	public override void Update () {
+		targetPos = GameObject.FindGameObjectWithTag("Player").transform.position;
 		Vector3 moveVec = targetPos - agent.gameObject.transform.position;
 		moveVec.Normalize ();
 		agent.gameObject.transform.Translate (moveVec * agent.MoveSpeed);
