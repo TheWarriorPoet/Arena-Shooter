f12b6628d6cf81b35f67395aa5c21acadef7ab23
Tristan Garland <tristan.garland@students.aie.edu.au>
2015-06-21 16:44:54 +1000

    added a light also added a quick animation blend test It's not in the scene now though.

---
+++ Assets/PlayerProto/PlayerAnimationScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAnimationScript : MonoBehaviour {
+
+	// Use this for initialization
+	private Animator animator;
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (animator != null) {
+	
+			animator.SetFloat ("xVel", Input.GetAxis ("Horizontal"));
+			animator.SetFloat ("yVel", Input.GetAxis ("Vertical"));
+		} else
+			print ("fail");
+	}
+}
---
+++ Assets/PlayerProto/PlayerProto.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//-------------------------------------------------------------------------------------
+// Player
+// Class to control player input
+//-------------------------------------------------------------------------------------
+
+public class PlayerProto : Agent {
+
+    private Transform _myTransform = null;
+
+    public GameObject BulletPrefab = null;
+	private Animator _myAnimator;
+
+    public float RotateSpeed = 10.0f;
+    public float Invert = -1.0f;
+
+	// Use this for initialization
+	void Awake () {
+        _myTransform = transform;
+		_myAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ProcessInput();
+	}
+
+    void ProcessInput()
+    {
+		_myAnimator.SetFloat ("yVel", Input.GetAxis ("Horizontal")*Invert);
+		_myAnimator.SetFloat ("xVel", Input.GetAxis ("Vertical"));
+
+		float translation = Input.GetAxis("Vertical") * MoveSpeed;
+        float rotation = Input.GetAxis("Horizontal") * RotateSpeed * Invert;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        _myTransform.Translate(Input.GetAxis("Horizontal") * MoveSpeed*Time.deltaTime, 0f, translation);
+        //_myTransform.Rotate(0f, rotation,0f);
+		_myTransform.position = new Vector3 (_myTransform.position.x, 0f, _myTransform.position.z);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject Bullet = Instantiate(BulletPrefab);
+            Bullet bullet = Bullet.GetComponent<Bullet>();
+            bullet.Shoot(Input.mousePosition, _myTransform.position);
+        }
+    }
+}
