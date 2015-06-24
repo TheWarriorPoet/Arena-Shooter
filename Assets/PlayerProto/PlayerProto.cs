43a3758d0878c019b2073b3f5cd0e6cd5a18c50e
Tristan Garland <tristan.garland@students.aie.edu.au>
2015-06-24 18:41:19 +1000

    A few scene changes. camera follows, using spring joint. not great and probably need to be replaced with a script.

--- Assets/PlayerProto/PlayerProto.cs
+++ Assets/PlayerProto/PlayerProto.cs
@@ -7,45 +7,59 @@ using System.Collections;
 //-------------------------------------------------------------------------------------
 
 public class PlayerProto : Agent {
-
-    private Transform _myTransform = null;
-
-    public GameObject BulletPrefab = null;
-	private Animator _myAnimator;
-
-    public float RotateSpeed = 10.0f;
-    public float Invert = -1.0f;
-
+	
+	private Transform _myTransform = null;
+	private Animator _myAnimator = null;
+	private Rigidbody _myRigidbody = null;
+	private CharacterController _myCharacterController = null;
+	
+	public GameObject BulletPrefab = null;
+	public float RotateSpeed = 10.0f;
+	public float Invert = -1.0f;
+	
 	// Use this for initialization
-	void Awake () {
-        _myTransform = transform;
+	void Awake ()
+	{
+		_myTransform = transform;
 		_myAnimator = GetComponent<Animator>();
+		//_myRigidbody = GetComponent<Rigidbody>();
+		_myCharacterController = GetComponent<CharacterController> ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        ProcessInput();
+	void Update ()
+	{
+		ProcessInput();
 	}
-
-    void ProcessInput()
-    {
+	
+	void ProcessInput()
+	{
+		
+		float xTranslation = Input.GetAxis ("Horizontal"); 
+		float zTranslation = Input.GetAxis ("Vertical"); 
+		float rotation = Input.GetAxis ("Horizontal") * RotateSpeed * Invert;
+		
+		Vector3 movement = new Vector3 (xTranslation, 0f, zTranslation);
+		movement = Vector3.ClampMagnitude(movement,1.0f) * MoveSpeed * Time.deltaTime;
+		
+		_myCharacterController.Move (movement);
+		
+		rotation *= Time.deltaTime;
+		
+		//_myTransform.Rotate(0, rotation, 0);
+		
+		// Temporary animation stuff
 		_myAnimator.SetFloat ("yVel", Input.GetAxis ("Horizontal")*Invert);
 		_myAnimator.SetFloat ("xVel", Input.GetAxis ("Vertical"));
-
-		float translation = Input.GetAxis("Vertical") * MoveSpeed;
-        float rotation = Input.GetAxis("Horizontal") * RotateSpeed * Invert;
-        translation *= Time.deltaTime;
-        rotation *= Time.deltaTime;
-
-        _myTransform.Translate(Input.GetAxis("Horizontal") * MoveSpeed*Time.deltaTime, 0f, translation);
-        //_myTransform.Rotate(0f, rotation,0f);
+		
+		// bad but be required with final character
 		_myTransform.position = new Vector3 (_myTransform.position.x, 0f, _myTransform.position.z);
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            GameObject Bullet = Instantiate(BulletPrefab);
-            Bullet bullet = Bullet.GetComponent<Bullet>();
-            bullet.Shoot(Input.mousePosition, _myTransform.position);
-        }
-    }
+		
+		if (Input.GetMouseButtonDown(0))
+		{
+			GameObject Bullet = Instantiate(BulletPrefab);
+			Bullet bullet = Bullet.GetComponent<Bullet>();
+			bullet.Shoot(Input.mousePosition, _myTransform.position);
+		}
+	}
 }
--- Assets/Resources/Scripts/Player.cs
+++ Assets/Resources/Scripts/Player.cs
@@ -7,19 +7,19 @@ using System.Collections;
 //-------------------------------------------------------------------------------------
 
 public class Player : Agent {
-
-    private Transform _myTransform = null;
+	
+	private Transform _myTransform = null;
 	private Animator _myAnimator = null;
 	private Rigidbody _myRigidbody = null;
-    public GameObject BulletPrefab = null;
-
-    public float RotateSpeed = 10.0f;
-    public float Invert = -1.0f;
-
+	public GameObject BulletPrefab = null;
+	
+	public float RotateSpeed = 10.0f;
+	public float Invert = -1.0f;
+	
 	// Use this for initialization
 	void Awake ()
 	{
-        _myTransform = transform;
+		_myTransform = transform;
 		_myAnimator = GetComponent<Animator>();
 		_myRigidbody = GetComponent<Rigidbody>();
 	}
@@ -27,36 +27,36 @@ public class Player : Agent {
 	// Update is called once per frame
 	void Update ()
 	{
-        ProcessInput();
+		ProcessInput();
 	}
-
-    void ProcessInput()
-    {
+	
+	void ProcessInput()
+	{
 		_myRigidbody.velocity = Vector3.zero;
-
+		
 		float xTranslation  = Input.GetAxis("Horizontal") * MoveSpeed;
 		float zTranslation = Input.GetAxis("Vertical") * MoveSpeed;
 		float rotation = Input.GetAxis ("Horizontal") * RotateSpeed * Invert;
-
+		
 		xTranslation *= Time.deltaTime;
 		zTranslation *= Time.deltaTime;
-        rotation *= Time.deltaTime;
-
+		rotation *= Time.deltaTime;
+		
 		_myTransform.Translate(xTranslation, 0, zTranslation);
 		//_myTransform.Rotate(0, rotation, 0);
-
+		
 		// Temporary animation stuff
 		_myAnimator.SetFloat ("yVel", Input.GetAxis ("Horizontal")*Invert);
 		_myAnimator.SetFloat ("xVel", Input.GetAxis ("Vertical"));
-
+		
 		// bad bad but be aproblem with final character
 		_myTransform.position = new Vector3 (_myTransform.position.x, 0f, _myTransform.position.z);
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            GameObject Bullet = Instantiate(BulletPrefab);
-            Bullet bullet = Bullet.GetComponent<Bullet>();
-            bullet.Shoot(Input.mousePosition, _myTransform.position);
-        }
-    }
+		
+		if (Input.GetMouseButtonDown(0))
+		{
+			GameObject Bullet = Instantiate(BulletPrefab);
+			Bullet bullet = Bullet.GetComponent<Bullet>();
+			bullet.Shoot(Input.mousePosition, _myTransform.position);
+		}
+	}
 }
