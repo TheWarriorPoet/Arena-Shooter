7893869bea2e2b592e6002f2877a8210bccc9cf5
Zyte <gcheffirs@gmail.com>
2015-06-25 19:04:24 +0930

    Menu: start game with keyboard/controller

--- Assets/Resources/Scripts/SceneManager_MainMenu.cs
+++ Assets/Resources/Scripts/SceneManager_MainMenu.cs
@@ -1,16 +1,22 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SceneManager_MainMenu : SceneManager_Base {
-
+public class SceneManager_MainMenu : SceneManager_Base
+{
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-    
+	void Update ()
+	{
+		// Quick hack to go to game with controller/keyboard
+    	if (Input.GetAxis("Submit") > 0)
+		{
+			LoadGame();
+		}
     }
 
     public void LoadGame()
