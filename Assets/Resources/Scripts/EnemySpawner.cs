b7b8ed88ccb1389deb652a8ae7f77fdb846edab6
Mitchell Osborne <mitchell.osborne@students.aie.edu.au>
2015-06-25 19:37:11 +1000

    Created the Enemy Spawner Object, spawns enemy units at a rate specified by the user, Also cleaned up some unused assets.

---
+++ Assets/Resources/Scripts/EnemySpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+	public float spawnDelay = 0.5f;
+	float spawnCounter = 0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		spawnCounter += Time.deltaTime;
+		if (spawnCounter >= spawnDelay) {
+			GameObject EnemyObj =(GameObject)Resources.Load("Prefabs/Enemy");
+			GameObject.Instantiate(EnemyObj, gameObject.transform.position,Quaternion.identity);
+			spawnCounter = 0;
+		}
+	}
+}
