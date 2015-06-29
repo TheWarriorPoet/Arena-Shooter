10d41eec493dc350e8b7814b721b810bdbed1ed2
Zyte <gcheffirs@gmail.com>
2015-06-29 17:42:35 +0930

    Replaced spring with CameraController script
    The movement can be a bit buggy atm, will try and fix later. This script
    makes it much more customizable though.

---
+++ Assets/Resources/Scripts/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject target;
+	public float velocityRate;
+
+	private CharacterController cc;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start()
+	{
+		offset = transform.position - target.transform.position;
+
+		cc = target.GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		Vector3 vel = cc.velocity;
+		vel.y = 0;
+
+		transform.position = target.transform.position + offset - (vel * velocityRate);
+	}
+}
