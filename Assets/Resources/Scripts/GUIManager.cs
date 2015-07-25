4e7611159e842d973bf9cb7e8bdf77b02f1499d3
Zyte <gcheffirs@gmail.com>
2015-07-26 00:45:10 +0930

    Hud now displays, health bar working for now
    Things will change once the ammo system and health/lives system is
    adjusted

---
+++ Assets/Resources/Scripts/GUIManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GUIManager : MonoBehaviour
+{
+	public GameObject player;
+	public GameObject health;
+	public GameObject ammo;
+	public GameObject[] life;
+
+	private PlayerProto p;
+	private RectTransform hRect;
+	private RectTransform aRect;
+	private float hWidth;
+	private float aWidth;
+	private float maxHP;
+
+	// Use this for initialization
+	void Start()
+	{
+		p = player.GetComponent<PlayerProto>();
+		hRect = health.GetComponent<RectTransform>();
+		aRect = ammo.GetComponent<RectTransform>();
+		hWidth = hRect.sizeDelta.x;
+		aWidth = aRect.sizeDelta.x;
+
+		maxHP = p.HP;
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		Vector2 newSize = hRect.sizeDelta;
+
+		newSize.x = hWidth * p.HP / maxHP;
+
+		hRect.sizeDelta = newSize;
+	}
+}
