69fd12660abbb8b96de368a39071b70b6c44d909
Mitchell Osborne <mitchell.osborne@students.aie.edu.au>
2015-07-30 12:27:34 +1000

    Added a spawn system... but it looks as though someone may have beaten me to it?

--- Assets/Resources/Scripts/EnemySpawner.cs
+++ Assets/Resources/Scripts/EnemySpawner.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class EnemySpawner : MonoBehaviour {
 	public float spawnDelay = 0.5f;
 	float spawnCounter = 0f;
-	public float spawnCap = 200;
+	//public float spawnCap = 200;
 	// Use this for initialization
 	void Start () {
 	
@@ -13,11 +13,11 @@ public class EnemySpawner : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		spawnCounter += Time.deltaTime;
-		if (spawnCounter >= spawnDelay && gameObject.GetComponentInParent<EnemyManager> ().numEnemies < spawnCap) {
+		if (spawnCounter >= spawnDelay && gameObject.GetComponentInParent<EnemyManager> ().canSpawn) {
+            gameObject.GetComponentInParent<EnemyManager>().numEnemies += 1;
 			GameObject tempObj =(GameObject)Resources.Load("Prefabs/Enemy");
 			GameObject EnemyObj = (GameObject)Instantiate(tempObj, gameObject.transform.position,Quaternion.identity);
 			EnemyObj.transform.SetParent(transform.parent);
-			gameObject.GetComponentInParent<EnemyManager>().numEnemies += 1;
 			spawnCounter = 0;
 		}
 	}
