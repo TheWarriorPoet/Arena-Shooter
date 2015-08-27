ad025c80cee227852ff5eb52ead452812a183905
zyte <zyte@zytebyte.com>
2015-08-27 19:46:41 +0930

    Manager working for one pickup

--- Assets/Resources/Scripts/PickupManager.cs
+++ Assets/Resources/Scripts/PickupManager.cs
@@ -3,13 +3,30 @@ using System.Collections;
 
 public class PickupManager : MonoBehaviour
 {
+	public GameObject prefab;
+	public GameObject spawnPoint;
+	public float spawnTime;
+
+	private GameObject spawnedObject;
+	private float timer;
+
 	void Start()
 	{
-		
+		timer = spawnTime;
 	}
 	
 	void Update()
 	{
-		
+		if (spawnedObject == null)
+		{
+			timer += Time.deltaTime;
+
+			if (timer >= spawnTime)
+			{
+				spawnedObject = (GameObject)Instantiate(prefab, spawnPoint.transform.position, spawnPoint.transform.rotation);
+
+				timer = 0;
+			}
+		}
 	}
 }
