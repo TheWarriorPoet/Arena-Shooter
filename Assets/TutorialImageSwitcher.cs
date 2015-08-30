d1dc600edc0c0f6c1d590fb9b7e66b45adf11ae5
Weevilman <weevilman@live.com>
2015-08-31 00:48:49 +1000

    Adding Tutorial Scene and Assets.
    Added tutorial scene, with script to switch images through the list,
    then proceed to the designated Level, Currently set to MainGame, may
    need adjustment when added to Level Flow.

---
+++ Assets/TutorialImageSwitcher.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TutorialImageSwitcher : MonoBehaviour {
+	public Sprite[] Pages;
+	public Image DisplayImage;
+	public int PageIndex = 0;
+	public string LevelName;
+
+	// Use this for initialization
+	void Start () {
+		DisplayImage.sprite = Pages [0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Nextimage(){
+
+		if (PageIndex + 1 < Pages.Length) {
+			PageIndex = PageIndex + 1;
+			DisplayImage.sprite = Pages[PageIndex];
+
+		} else {
+			Application.LoadLevel (LevelName);
+		}
+
+
+		//PageIndex = PageIndex + 1;
+		//Pages.Length
+
+	}
+}
